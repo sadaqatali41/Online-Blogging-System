2602bbdb885de594d3c667721de2f031b3a2dae6
Sadaqat Ali <36379399+sadaqatali41@users.noreply.github.com>
2019-03-05 17:01:43 +0530

    Update addarticlesview.php

--- CI Project/application/views/addarticlesview.php
+++ CI Project/application/views/addarticlesview.php
@@ -15,7 +15,7 @@
 			</div>
 			<div class="row">
 				<div class="col-sm-12 text-center">
-							<strong><?php echo form_error('title'); ?></strong>
+					<strong><?php echo form_error('title'); ?></strong>
 				</div>
 			</div>
 			<br>
@@ -36,7 +36,7 @@
 			</div>
 			<div class="row">
 				<div class="col-sm-12 text-center">
-							<strong><?php if(isset($upload_error)){echo $upload_error;} ?></strong>
+					<strong><?php if(isset($upload_error)){echo $upload_error;} ?></strong>
 				</div>
 			</div>
 			<br>
